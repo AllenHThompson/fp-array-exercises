e684ebbe8b331d9ef8118141b2d9a8e9224e8e2f
Allen H. Thompson <allenhthompson1@gmail.com>
2016-06-07 08:56:19 -0400

    Updated exercises.

---
+++ fp-array-exercises.js
@@ -0,0 +1,11 @@
+//USE FILTER TO RETURN POSITIVE NUMBERS OUT OF //AN ARRAY
+var num = [-1, 4, -3, 5, 10, -9];
+var posNum = num.filter(function(num) {
+     return num > 0;
+});
+
+//USE MAP AND RETURN AN ARRAY WITH EACH //NUMBER SQUARED
+var num = [2, 4, 6, 9];
+var squNum = num.map(function(num) {
+     return element * element;
+});
